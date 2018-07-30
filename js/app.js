930ced10469e90faf77f080dd30b24704efc8baf
Deise Trianon <deisetrianon@gmail.com>
2018-07-29 22:31:06 -0300

    adicionei mapa

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// La funcionalidad de tu proyecto
+/* Google Maps API */
+var map;
+var mapMobile;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -11.606968, lng: -77.059364},
+    zoom: 3
+  });
+
+  mapMobile = new google.maps.Map(document.getElementById('map-mobile'), {
+    center: {lat: -11.606968, lng: -77.059364},
+    zoom: 3
+  });
+}
+
